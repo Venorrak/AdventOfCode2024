a6778e06a65e1b9e4e9a9df70a01f9dca14db148
venorrak <jaudet1909@gmail.com>
2024-12-03 00:37:00 -0500

    in the end I finished it all rn day3

--- day3/day3.rb
+++ day3/day3.rb
@@ -6,11 +6,49 @@ def createReport(file_path)
   File.read(file_path).split("\n").join("")
 end
 
+def getDoDont(input)
+  do_regex = /do\(\)/
+  dont_regex = /don't\(\)/
+  dos = input.scan(do_regex)
+  donts = input.scan(dont_regex)
+  doPos = input.enum_for(:scan, do_regex).map { Regexp.last_match.begin(0) }
+  dontPos = input.enum_for(:scan, dont_regex).map { Regexp.last_match.begin(0) }
+  doWithPos = []
+  # create an array of hashes containing the do as true and don't as false in order of pos
+  (dos.count + donts.count).times do |i|
+    if doPos[0].nil?
+      doWithPos.push({do: false, pos: dontPos[0]})
+      dontPos.shift
+      next
+    elsif dontPos[0].nil?
+      doWithPos.push({do: true, pos: doPos[0]})
+      doPos.shift
+      next
+    end
+    if doPos[0] < dontPos[0]
+      doWithPos.push({do: true, pos: doPos[0]})
+      doPos.shift
+    else
+      doWithPos.push({do: false, pos: dontPos[0]})
+      dontPos.shift
+    end
+  end
+  return doWithPos
+end
+
 def getMuls(input)
   regex = /mul\(\d{1,3},\d{1,3}\)/
   muls = input.scan(regex)
   mulsPos = input.enum_for(:scan, regex).map { Regexp.last_match.begin(0) }
-  return muls
+  mulsWithPos = []
+  muls.each_with_index do |mul, i|
+    mulWithPos = {
+      mul: mul,
+      pos: mulsPos[i]
+    }
+    mulsWithPos.push(mulWithPos)
+  end
+  return mulsWithPos
 end
 
 def solveMul(mul)
@@ -22,8 +60,13 @@ end
 
 input = createReport("input3.txt")
 muls = getMuls(input)
-total = 0
+doDont = getDoDont(input)
+result = 0
+
 muls.each do |mul|
-  total += solveMul(mul)
+  lastDoDont = doDont.select { |doDont| doDont[:pos] < mul[:pos] }.last
+  if lastDoDont.nil? || lastDoDont[:do]
+    result += solveMul(mul[:mul])
+  end
 end
-p total
+p result
